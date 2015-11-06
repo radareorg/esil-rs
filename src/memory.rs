4a00fc687ede35d8317e8482fd652117f6fe1e7e
Sushant <sushant.dinesh94@gmail.com>
2015-11-07 02:28:04 +0530

    Temporary commit

--- src/memory.rs
+++ src/memory.rs
@@ -8,13 +8,20 @@
 use std::ops::{Add, BitAnd, BitOr, BitXor, Index, Mul, Not, Rem, Shl, Shr, Sub};
 use std::collections::BTreeMap;
 use std::rc::Rc;
-use std::cell::RefCell;
+use std::cell::{Cell, RefCell};
 use std::hash::{Hash, Hasher};
 use std::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
 
 pub trait BasicOps: Add + BitAnd + BitOr + BitXor + Mul + Not + Rem
                     + Shl<Self> + Shr<Self> + Sub + Sized {}
 
+pub trait MemoryOps {
+    type DataType;
+    fn read(&self, address: u64, size: usize) -> Self::DataType;
+    fn write(&self, address: u64, size: usize, data: Self::DataType);
+    // Nop by default
+    fn dump_memory(&self) { }
+}
 
 // Some notes on design.
 // Chunks are assumed to be of fixed size, that is 8 bytes (as our primary
@@ -48,18 +55,10 @@ pub trait BasicOps: Add + BitAnd + BitOr + BitXor + Mul + Not + Rem
 //  TODO: Be able to create chunks of arbitrary size.
 
 // Internally used associate addresses with the locations in the buffers.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 struct MemMetaData {
     start: u64,
-    size: usize,
-}
-
-// Represents the result struct for find_memory_by_address.
-// Returns the MemMetaData that the address is a part of and the offset within that chunk.
-struct FindResult {
-    meta: MemMetaData,
-    offset: usize,
-    needs_allocation: bool,
+    size: Cell<usize>,
 }
 
 impl Hash for MemMetaData {
@@ -92,31 +91,49 @@ impl MemMetaData {
     pub fn new(start: u64, size: usize) -> MemMetaData {
         MemMetaData {
             start: start,
-            size: size,
+            size: Cell::new(size),
         }
+    }
+
+    pub fn size(&self) -> usize {
+        self.size.get()
+    }
+
+    pub fn sizeu64(&self) -> u64 {
+        self.size.get() as u64
+    }
+
+    pub fn extend_by(&self, n: usize) {
+        self.size.set(self.size() + n);
     }
 }
 
-// Actual Chunk of that represents memory
-pub struct MemoryChunk<T: BasicOps> {
+// Represents the result struct for find_memory_by_address.
+// Returns the MemMetaData that the address is a part of and the offset within that chunk.
+struct FindResult {
     meta: MemMetaData,
+    offset: usize,
+    needs_allocation: bool,
+}
+
+// Actual Chunk of that represents memory
+pub struct _MemoryChunk<T: BasicOps> {
     // next chunk is used to continue writes into the next chunk. This field is a `None`
     // if there is no chunk that continues the present chunk.
     // This field is basically used to coalesce two adjacent chunks together.
-    next_chunk: Option<Rc<RefCell<Vec<T>>>>,
-    prev_chunk: Option<Rc<RefCell<Vec<T>>>>,
+    next_chunk: Option<MemoryChunk<T>>,
     buffer: Rc<RefCell<Vec<T>>>,
     // For later use. To mark that a memory has been written out to disk and can be reused.
     in_use: bool,
 }
 
-impl<T: BasicOps> MemoryChunk<T> {
-    fn new(meta: MemMetaData) -> MemoryChunk<T> {
+type MemoryChunk<T> = Rc<RefCell<_MemoryChunk<T>>>;
+
+impl<T: BasicOps> _MemoryChunk<T> {
+    fn new() -> _MemoryChunk<T> {
         let buffer = Rc::new(RefCell::new(Vec::new()));
-        MemoryChunk {
-            meta: meta,
+        _MemoryChunk {
             next_chunk: None,
-            prev_chunk: None,
             buffer: buffer,
             in_use: true,
         }
@@ -127,7 +144,7 @@ impl<T: BasicOps> MemoryChunk<T> {
 pub struct MemoryRegion<T: BasicOps> {
     min: u64,
     max: u64,
-    allocated: BTreeMap<MemMetaData, Box<MemoryChunk<T>>>,
+    allocated: BTreeMap<MemMetaData, MemoryChunk<T>>,
 }
 
 impl<T: BasicOps> MemoryRegion<T> {
@@ -145,17 +162,17 @@ impl<T: BasicOps> MemoryRegion<T> {
         }
         let mut keys = self.allocated.keys().collect::<Vec<&MemMetaData>>();
         while let Some(key) = keys.pop() {
-            if meta.start >= key.start && meta.start < key.start + key.size as u64 {
+            if meta.start >= key.start && meta.start < key.start + key.sizeu64() {
                 let offset = meta.start - key.start;
-                let needs_allocation = (meta.start + meta.size as u64) >
-                                       (key.start + key.size as u64);
+                let needs_allocation = (meta.start + meta.sizeu64()) >
+                                       (key.start + key.sizeu64());
                 return Some(FindResult {
                     meta: key.clone(),
                     offset: offset as usize,
                     needs_allocation: needs_allocation,
                 });
             }
-            if (key.start + key.size as u64) < meta.start {
+            if key.start + key.sizeu64() < meta.start {
                 break;
             }
         }
@@ -164,9 +181,9 @@ impl<T: BasicOps> MemoryRegion<T> {
 
     // Returns true if the two memory locations are adjacent to each other
     fn is_adjacent(mem: &MemMetaData, mem_: &MemMetaData) -> bool {
-        if mem.start == mem_.start + mem_.size as u64 + 1 {
+        if mem.start == mem_.start + mem_.sizeu64() + 1 {
             true
-        } else if mem_.start == mem.start + mem.size as u64 + 1 {
+        } else if mem_.start == mem.start + mem.sizeu64() + 1 {
             true
         } else {
             false
@@ -174,58 +191,70 @@ impl<T: BasicOps> MemoryRegion<T> {
     }
 
     // Tries and coalesce all chunks that are possible.
-    // TODO: Match sizes. Remove next_key if coalesced with the previous chunk.
-    // Coalesce memory chunk together rather than just the buffers.
     fn coalesce_chunks(&mut self) {
-        let mut keys = self.allocated.iter_mut().peekable();
-        if let Some((mut key, mut cur_chunk)) = keys.next() {
-            loop {
-                if let Some((next_key, mut next_chunk)) = keys.next() {
-                    if !MemoryRegion::<T>::is_adjacent(key, next_key) {
+        let mut compacted = Vec::<MemMetaData>::new();
+        {
+            let mut keys = self.allocated.iter_mut().peekable();
+            if let Some((mut key, mut cur_chunk)) = keys.next() {
+                loop {
+                    if let Some((next_key, mut next_chunk)) = keys.next() {
+                        if !MemoryRegion::<T>::is_adjacent(key, next_key) {
+                            key = next_key;
+                            cur_chunk = next_chunk;
+                            continue;
+                        }
+
+                        assert!(cur_chunk.borrow().next_chunk.is_none());
+                        (*cur_chunk).borrow_mut().next_chunk = Some(next_chunk.clone());
+                        key.extend_by(next_key.size());
+
                         key = next_key;
                         cur_chunk = next_chunk;
+
+                        compacted.push(next_key.clone());
                         continue;
                     }
-                    if cur_chunk.next_chunk.is_none() {
-                        (*cur_chunk).next_chunk = Some(next_chunk.buffer.clone());
-                    }
-                    if next_chunk.prev_chunk.is_none() {
-                        (*next_chunk).prev_chunk = Some(cur_chunk.buffer.clone());
-                    }
-                    key = next_key;
-                    cur_chunk = next_chunk;
-                    continue;
+                    break;
                 }
-                break;
             }
+        }
+        for key in &compacted {
+            self.allocated.remove(key);
+        }
+    }
+    
+    fn new_chunk(&mut self, meta: &MemMetaData) {
+        let (allocation_size, allocation_start) = match self.find_memory_by_address(meta) {
+            Some(ref res) => {
+                match res.needs_allocation {
+                    true => (meta.size() - res.meta.size() + res.offset,
+                              res.meta.start + res.meta.sizeu64()),
+                    false => (0, 0),
+                }
+            },
+            None => (meta.size(), meta.start),
+        };
+        if allocation_size > 0 {
+            let new_meta = MemMetaData::new(allocation_start, allocation_size);
+            let chunk = Rc::new(RefCell::new(_MemoryChunk::new()));
+            self.allocated.insert(new_meta, chunk);
+            self.coalesce_chunks();
         }
     }
 
-    // Three steps to make a new chunk.
-    //  - Check if the chunk already exists. If not, needs_allocation
-    //  - Check if the new chunk requested for is a continuation of some other chunk, i.e. the
-    //    memory can be made contiguous.
-    //  - Allocate and coalesce if necessary
-    fn new_chunk(&mut self, meta: &MemMetaData) {
-        let mut allocation_size = meta.size;
-        let mut allocation_start = meta.start;
-        match self.find_memory_by_address(meta) {
-            Some(ref res) => {
-                if !res.needs_allocation {
-                    return;
-                }
-                allocation_size = meta.size - (res.meta.size - res.offset);
-                allocation_start = res.meta.start + res.meta.size as u64;
-            },
-            None => {},
-        }
-        // Allocate the new chunk.
-        let new_meta = MemMetaData::new(allocation_start, allocation_size);
-        let chunk = Box::new(MemoryChunk::new(new_meta));
-        self.allocated.insert(new_meta, chunk);
-        self.coalesce_chunks();
+    pub fn read(&self, address: u64, size: usize) -> Option<&[T]> {
+        unimplemented!()
+    }
+
+    pub fn write(&mut self, address: u64, size: usize, data: &[T]) {
+        unimplemented!()
+    }
+
+    pub fn dump_memory(&self) {
+        unimplemented!()
     }
 }
+
 
 #[cfg(test)]
 mod test {
